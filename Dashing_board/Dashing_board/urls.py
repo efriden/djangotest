76477b8a4c5cc7b706646f1b355a516ffc9e0d4a
Eric Fridén <eric.friden@gmail.com>
2018-10-31 21:52:25 +0100

    First!

---
+++ Dashing_board/Dashing_board/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for Dashing_board.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', Dashing_board.views.home, name='home'),
+    # url(r'^Dashing_board/', include('Dashing_board.Dashing_board.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
